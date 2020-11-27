246c37cde552f1dfbf3320aadf340b5d257e2094
‘yzq-aaa’ <2581569105@qq.com>
2020-11-27 14:45:27 +0800

    '14.45'

--- js/public.js
+++ js/public.js
@@ -1,6 +1,6 @@
 function setRem() {
     var win = document.documentElement.clientWidth || document.body.clientWidth;
-    var uiw = 320;
+    var uiw = 750;
     document.documentElement.style.fontSize = (win / uiw) * 1 + 'px'
 }
 setRem();

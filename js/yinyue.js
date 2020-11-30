cd07dac8eab69ed8b2a3f381548b0eccebb9364c
‘yzq-aaa’ <2581569105@qq.com>
2020-11-30 12:55:37 +0800

    '12.54'

--- js/yinyue.js
+++ js/yinyue.js
@@ -1,94 +1,103 @@
-var video=document.getElementById('video');
-var jpplay=document.getElementsByClassName('jp-play')[0]
-var jppause=document.getElementsByClassName('jp-pause')[0]
+var arr=0;
+$('.hua').on('touchstart',function (){
+var d= $('audio'); 
+  arr++;
+  num=1;
+  if(arr>=d.length){
+    arr=0
+}
+d.attr('class','audio1')
+d[arr].className='audio';
+var gm=$('.audio').text()
+$('.text p').eq('0').text(gm)
+$('.text p').eq('0').removeClass('rl')
+$('.text p').eq('0').addClass('rl')
+
+paly();
+clearInterval(timer)
+setTimeout(function(){
+  go(); 
+},1000)
+}) 
+
+$('.imgs img').on('touchstart',function(){
+  
+  if( $('.sol').is(':hidden')){
+   $('.sol').show('slow')
+  }else{
+    $('.sol').slideUp('slow')
+  }
+})
+$('.sol ul li').on('touchstart',function(){
+  $('.sol ul li').removeClass('rgb')
+  $(this).addClass('rgb')
+})
+$('.sol ul li').on('touchend',function(){
+  $('.sol ul li').removeClass('rgb')
+  var index=$(this).index()
+  var dio= $('audio'); 
+  dio.attr('class','audio1')
+  dio[index].className='audio';
+  var gm=$('.audio').text()
+  $('.text p').eq('0').text(gm)
+  $('.text p').eq('0').removeClass('rl')
+  $('.text p').eq('0').addClass('rl')
+  num=1;
+  paly();
+  clearInterval(timer)
+  setTimeout(function(){
+  go(); 
+},1000)
+
+})
+
+var jpplay=$('.jp-play')[0]
+var jppause=$('.jp-pause')[0]
+var jpduration=$('.jp-duration')[0]
+var jpcurrenttime=$('.jp-current-time')[0]
 
 
-var jpduration=document.getElementsByClassName('jp-duration')[0]
-var jpcurrenttime=document.getElementsByClassName('jp-current-time')[0]
 
 
-var jpseekbar=document.getElementsByClassName('jp-seek-bar')[0]
-var jpplaybar=document.getElementsByClassName('jp-play-bar')[0]
-var jpfullscreen=document.getElementsByClassName('jp-full-screen')[0]
 
-var jpvolumebarvalue=document.getElementsByClassName('jp-volume-bar-value')[0]
-var jpvolumebar=document.getElementsByClassName('jp-volume-bar')[0]
-var b=0;
-var span=document.getElementsByTagName('span')[0]
-video.addEventListener('canplay',function(){
-    jpplay.addEventListener('click',function(){
-        
-      video.play();  
+function paly(){
+     var audio=$('.audio')[0]; 
+     var audio1=$('.audio1')[0]; 
+     console.log(audio1)
+     console.log(audio)
+    
+     jpplay.addEventListener('click',function(){
+     audio1.pause();
+     audio.play();  
      jpplay.style="display:none"
-     jppause.style="display:block"
-     
-    })
-
-    jppause.addEventListener('click',function(){
-      video.pause();  
+     jppause.style="display:block" 
+     }) 
+     setTimeout(function(){
+      audio1.pause();
+      audio.play();  
+      jpplay.style="display:none";
+      jppause.style="display:block" ;
+     },1000)
+     jppause.addEventListener('click',function (){
+     audio.pause();  
      jpplay.style="display:block"
      jppause.style="display:none"
     });
+
+//     setInterval(function(){
+//     jpcurrenttime.innerText=formatTime(audio.currentTime);  
+//     },1000)
+//     jpduration.innerText=formatTime(audio.duration); 
     
-    jpduration.innerText=formatTime(video.duration);
-   video.addEventListener('timeupdate',function(){
-     jpcurrenttime.innerText=formatTime(video.currentTime) 
-     jpplaybar.style.width=(video.currentTime / video.duration)*100+'%' 
-   })
-    jpseekbar.addEventListener('click',function(e){
-        e=e||window.event;
-        var cx=e.offsetX;
-        var cw=this.offsetWidth;
-
-        jpplaybar.style.width=(cx/cw)*100+'%';
-
-        video.currentTime = (cx / cw)*video.duration;
-
-    })
-    jpfullscreen.addEventListener('click',function(){
-        video.webkitRequestFullScreen();
-    })
-
-
-
-    video.addEventListener('ended',function(){
-        video.currentTime=0;
-        jpplaybar.style="width=0%";
-        jppause.style="display:none";
-        jpplay.style="display:block";
-    })
-    jpvolumebar.onclick = function(e) {
-        var e = e || window.event;
-        jpvolumebarvalue.style.width = Math.floor(e.offsetX / this.offsetWidth * 100) + '%';
-        video.volume = e.offsetX / this.offsetWidth
-    }
     
-    document.onkeydown =function(e){
-        var e=e|| window.event;
-        b++;
-        if(b%2==1){
-        if(e.keyCode==32){
-          video.play();  
-            jpplay.style="display:none"
-            jppause.style="display:block" 
-        }
-        }else{
-           
-              video.pause();  
-            jpplay.style="display:block"
-            jppause.style="display:none"
-        }        
-    }
-});
-function formatTime(t){
-    var m =Math.floor(t/60)
-    m<10?s='0'+m:m;
-    var s =Math.floor(t%60);
-    s<10?s='0'+s:s;
-    return  m+':'+s;
+   
+   
+// function formatTime(t){
+//     var m =Math.floor(t/60)
+//     m<10?s='0'+m:m;
+//     var s =Math.floor(t%60);
+//     s<10?s='0'+s:s;
+//     return  m+':'+s;
+// }
 }
-// setInterval(function(){
-//     span.style.background='rgb('+parseInt(Math.random()*256)+','+parseInt(Math.random()*256)+','+parseInt(Math.random()*256)+')'
-// },1000)
-
 
